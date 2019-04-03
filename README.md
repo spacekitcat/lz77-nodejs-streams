231b5c7ca1f1332ed76064acb4bb76a3f4eef6b4
Lisa Burton <Lisa.Burton01@bbc.co.uk>
2019-04-03 23:28:34 +0100

    feat(sample): Adding snazzy progress bar and some documentation.

--- README.md
+++ README.md
@@ -2,6 +2,28 @@
 
 Compression and decompression streams for the Lempel-Ziv 77 compression algorithm. The compression stream convert streams of bytes into a stream of JSON objects that represent the data in their compressed form. The decompression stream produces the input byte stream from the JSON objects. This is designed as a dependency to another project (that doesn't exist yet) which will be responsible for serialization/de-serialization in a space efficient way. The prototype can be found here https://github.com/spacekitcat/libz77
 
+## Progress
+
+I've implemented the compression stream and it uses [Tiny Toatie Cache](https://github.com/spacekitcat/tiny-toatie-cache) as it's storage engine. In comparison with the original prototype, this version seems to be several times faster.
+
+## sample.js
+
+`sample.js` runs the compressor against any file path you give as the first argument. The `Saved` number is a tally of the number of bytes above 4 that the packet expands to, you would need about 4 bytes in overhead to serialize the packet to a file.
+
+```bash
+lz77-nodejs-streams ‹master*› % node sample.js resources/512m-test.txt
+[▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓░░░░░░░░░░] 67% | ETA: 266s | 362268672/536870912 | Saved: 344.13 MB (theoretical)
+
+    ... 266 seconds pass ...
+
+[▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓] 100% | ETA: 1s | 536870912/536870912 | Saved: 510 MB (theoretical)
+
+I compressed the devil outta resources/512m-test.txt
+
+             Input size: 536870912
+     Theoretical output: 2099778
+```
+
 ## Unit tests
 
 The unit tests use Jest and the Yarn command below runs them.
--- sample.js
+++ sample.js
@@ -2,13 +2,7 @@
 const LibzStream = require('./lib/libz-stream').default;
 const fs = require('fs');
 const process = require('process');
-const Spinner = require('cli-spinner').Spinner;
-
-var spinner = new Spinner('%s');
-spinner.setSpinnerString('★☆');
-spinner.setSpinnerDelay(1000);
-spinner.setSpinnerTitle('\x1b[31m  AWAITING DATA \x1b[0m');
-spinner.start();
+const _cliProgress = require('cli-progress');
 
 let filePath = process.argv[2];
 if (!filePath) {
@@ -18,14 +12,23 @@ if (!filePath) {
 let fileReadStream = fs.createReadStream(filePath, { highWaterMark: 1024 });
 const statsBefore = fs.statSync(filePath);
 
-const calculateProgress = (current, total) =>
-  Math.round((current / total) * 100, 2);
-
 let compressorTransformer = new LibzStream();
 let byteCount = 0;
 let saving = 0;
+const progressBar = new _cliProgress.Bar({
+  barCompleteChar: '▓',
+  barIncompleteChar: '░',
+  fps: 1,
+  stream: process.stdout,
+  barsize: 32,
+  position: 'right',
+  etaBuffer: 100,
+  format:
+    '[{bar}] {percentage}% | ETA: {eta}s | {value}/{total} | Saved: {saving} MB (theoretical)'
+});
+progressBar.start(statsBefore.size, 0, { saving: 0 });
+
 compressorTransformer.on('data', data => {
-  spinner.stop(true);
   byteCount += 1;
   if (data.prefix) {
     byteCount += data.prefix.length;
@@ -33,24 +36,18 @@ compressorTransformer.on('data', data => {
       saving += data.prefix.length - 3;
     }
   }
-  spinner.setSpinnerTitle(
-    `PROGRESS ${calculateProgress(
-      byteCount,
-      statsBefore.size
-    )}%, SAVINGS ${saving}`
-  );
-  spinner.start();
+  progressBar.update(byteCount, {
+    saving: Math.round((saving / 1024 / 1024) * 100) / 100
+  });
 });
 
 compressorTransformer.on('end', () => {
-  spinner.stop(false);
+  progressBar.stop();
   console.log();
   console.log(`I compressed the devil outta ${filePath}`);
-  //const statsAfter = fs.statSync(`${filePath}.bzz`);
   console.log();
-  console.log(`    Input size: ${statsBefore.size}`);
-  //console.log(`    Ouput size: ${statsAfter.size}`);
-  //console.log(`    IO   ratio: ${statsAfter.size / statsBefore.size}`);
+  console.log(`             Input size: ${statsBefore.size}`);
+  console.log(`     Theoretical output: ${statsBefore.size - saving}`);
   console.log();
 });
 
